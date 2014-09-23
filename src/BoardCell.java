aa5f652017504379556c2d5f2c4c26e6e1f62562
Michael Brown <mimbrown@mines.edu>
2014-09-23 17:30:24 -0600

    Methods that fail tests

---
+++ src/BoardCell.java
@@ -0,0 +1,6 @@
+
+public class BoardCell {
+	private int row;
+	private int column;
+
+}
---
+++ src/IntBoard.java
@@ -0,0 +1,32 @@
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Set;
+
+
+public class IntBoard {
+	private Map<BoardCell, Set<BoardCell>> adjacencies;
+	
+	public IntBoard() {
+		super();
+		
+	}
+	
+	public void calcAdjacencies() {
+		
+	}
+	
+	public void calcTargets() {
+		
+	}
+	
+	public Set<BoardCell> getTargets() {
+		return new HashSet<BoardCell>();
+	}
+	
+	public LinkedList<BoardCell> getAdjList(BoardCell b) {
+		return new LinkedList<BoardCell>();
+	}
+
+	
+}
