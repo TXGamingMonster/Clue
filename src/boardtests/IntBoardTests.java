e4e676c71b5d0cdcd528583b99cb39de7cae67ea
Jack Wesley Nelson <janelson@mymail.mines.edu>
2014-09-23 22:56:05 -0600

    changed the test for a roll of 4 and cell(1,1). I think it account for all targets properly now (was missing 2)

--- src/boardtests/IntBoardTests.java
+++ src/boardtests/IntBoardTests.java
@@ -26,7 +26,7 @@ public class IntBoardTests {
 		Assert.assertTrue(testList.contains(board.getCell(1,0)));
 		Assert.assertTrue(testList.contains(board.getCell(0,1)));
 		Assert.assertEquals(2, testList.size());
-		}
+	}
 	@Test
 	public void testAdjacencyTopRight(){
 		BoardCell cell = board.getCell(0,3);
@@ -34,7 +34,7 @@ public class IntBoardTests {
 		Assert.assertTrue(testList.contains(board.getCell(0,2)));
 		Assert.assertTrue(testList.contains(board.getCell(1,3)));
 		Assert.assertEquals(2, testList.size());
-		}
+	}
 	@Test
 	public void testAdjacencyBottomLeft(){
 		BoardCell cell = board.getCell(3,0);
@@ -42,7 +42,7 @@ public class IntBoardTests {
 		Assert.assertTrue(testList.contains(board.getCell(2,0)));
 		Assert.assertTrue(testList.contains(board.getCell(3,1)));
 		Assert.assertEquals(2, testList.size());
-		}
+	}
 	@Test
 	public void testAdjacencyBottomRight(){
 		BoardCell cell = board.getCell(3,3);
@@ -50,7 +50,7 @@ public class IntBoardTests {
 		Assert.assertTrue(testList.contains(board.getCell(2,3)));
 		Assert.assertTrue(testList.contains(board.getCell(3,2)));
 		Assert.assertEquals(2, testList.size());
-		}
+	}
 	@Test
 	public void testAdjacencyMidish(){
 		BoardCell cell = board.getCell(2,1);
@@ -60,8 +60,8 @@ public class IntBoardTests {
 		Assert.assertTrue(testList.contains(board.getCell(2,2)));
 		Assert.assertTrue(testList.contains(board.getCell(2,0)));
 		Assert.assertEquals(4, testList.size());
-		}
-	
+	}
+
 	@Test
 	public void testTargets0_3()
 	{
@@ -122,13 +122,15 @@ public class IntBoardTests {
 		BoardCell cell = board.getCell(1, 1);
 		board.calcTargets(cell, 4);
 		Set<BoardCell> targets = board.getTargets();
-		Assert.assertEquals(5, targets.size());
+		Assert.assertEquals(7, targets.size());
 		Assert.assertTrue(targets.contains(board.getCell(0, 0)));
 		Assert.assertTrue(targets.contains(board.getCell(2, 0)));
 		Assert.assertTrue(targets.contains(board.getCell(0, 2)));
 		Assert.assertTrue(targets.contains(board.getCell(2, 2)));
 		Assert.assertTrue(targets.contains(board.getCell(3, 3)));
+		Assert.assertTrue(targets.contains(board.getCell(3, 1)));
+		Assert.assertTrue(targets.contains(board.getCell(1, 3)));
 	}
-	
-	
+
+
 }
