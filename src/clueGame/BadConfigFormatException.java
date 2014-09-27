f2e8367716ca87f30b6d905016b372bfadb58e56
Jack Wesley Nelson <janelson@mymail.mines.edu>
2014-09-26 22:16:58 -0600

    error exception class updated.

--- src/clueGame/BadConfigFormatException.java
+++ src/clueGame/BadConfigFormatException.java
@@ -1,12 +1,25 @@
 package clueGame;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class BadConfigFormatException extends Exception{
+	String fileName = "log.txt";
 	public BadConfigFormatException() {
 		// TODO Auto-generated constructor stub
 	}
 	public BadConfigFormatException(String errorMessage) {
 		// TODO Auto-generated constructor stub
 		super(errorMessage);
+		try {
+		    PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(fileName, true)));
+		    out.println(errorMessage);
+		    out.close();
+		} catch (IOException e) {
+		    System.out.println(e.getMessage());
+		}
 	}
 	@Override
 	public String toString() {
