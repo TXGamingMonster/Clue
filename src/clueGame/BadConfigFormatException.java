e3a8c25280331514e0260a0eaf63b3808b39f68c
Jack Wesley Nelson <janelson@mymail.mines.edu>
2014-09-26 23:06:17 -0600

    altered format exception to log a message if not passed a custom error message

--- src/clueGame/BadConfigFormatException.java
+++ src/clueGame/BadConfigFormatException.java
@@ -8,7 +8,14 @@ import java.io.PrintWriter;
 public class BadConfigFormatException extends Exception{
 	String fileName = "log.txt";
 	public BadConfigFormatException() {
-		// TODO Auto-generated constructor stub
+		System.out.println("BadConfigFormatException thrown, correct the format of your configuration file to proceed.");
+		try {
+		    PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(fileName, true)));
+		    out.println("BadConfigFormatException thrown, correct the format of your configuration file to proceed.");
+		    out.close();
+		} catch (IOException e) {
+		    System.out.println(e.getMessage());
+		}
 	}
 	public BadConfigFormatException(String errorMessage) {
 		// TODO Auto-generated constructor stub
