352ffa01bfe8a754ac95b22102e5ca764903df82
Jack Wesley Nelson <janelson@mymail.mines.edu>
2014-09-28 22:33:33 -0600

    added boardData loading function, and moved this and the legend loading code to their own functions. LoadBoardConfig calls both of these.

--- src/clueGame/Board.java
+++ src/clueGame/Board.java
@@ -2,6 +2,8 @@ package clueGame;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
@@ -16,7 +18,9 @@ public class Board {
 	private int numColumns;
 	private Set<BoardCell> targets;
 	private String clueLegendFile = "ClueFilesUs/ClueLegend.txt";
-
+	private String clueBoardFile = "ClueFilesUs/BoardLayout.csv";
+	private ArrayList<String[]> boardData = new ArrayList<String[]>();
+	
 	public Board(){
 		try {
 			loadBoardConfig();
@@ -26,28 +30,80 @@ public class Board {
 			System.exit(0);
 		}
 	}
-	
+
 	public void loadBoardConfig() throws BadConfigFormatException {
 		//Load Legend into rooms map
+		loadLegend();
+		//Done loading legend, now load board
+		loadBoardData();
+	}
+	
+	private void loadBoardData() throws BadConfigFormatException{
+		FileReader clueBoardFileReader = null;
+		Scanner scanner = null;
+		try {
+			clueBoardFileReader = new FileReader(clueBoardFile);
+			scanner = new Scanner(clueBoardFileReader);
+		} catch (FileNotFoundException e) {
+			System.out.println("Exception: File Not Found: " + clueBoardFile + "\nExiting program.");
+			System.exit(0);
+		}
+		finally{
+			//each String[] in boardData is an array of the column data for the row at that index
+			while(scanner.hasNext()){
+				//the file should be comma delimited, so each row data is the line read in split by a comma, while the row is simply the line read in.
+				String[] toAdd = scanner.nextLine().split(",");
+				if(numColumns == 0){
+					//set column length equal to the first row that contains column data
+					numColumns = toAdd.length;
+				}
+				if(numColumns != toAdd.length){
+					throw new BadConfigFormatException(clueBoardFile + " is improperly formatted at row " + numRows+1 + ".");
+				}
+				//not sure how we should check if the row[column] data is valid.
+				boardData.add(toAdd);
+				numRows = boardData.size();//update number of rows that have been read in 
+			}
+		}
+		try {
+			clueBoardFileReader.close();
+		} catch (IOException e) {
+			System.out.println("Exception: File: " + clueBoardFile + " did not close properly.\nAttempting to continue..");
+			System.exit(0);
+		}
+	}
+	
+	private void loadLegend() throws BadConfigFormatException{
 		FileReader clueLegendFileReader = null;
+		Scanner scanner = null;
 		try {
 			clueLegendFileReader = new FileReader(clueLegendFile);
+			scanner = new Scanner(clueLegendFileReader);
 		} catch (FileNotFoundException e) {
 			System.out.println("Exception: File Not Found: " + clueLegendFile + "\nExiting program.");
 			System.exit(0);
 		}
-		Scanner scanner = new Scanner(clueLegendFileReader);
-		int clueLegendLinesRead = 0;
-		while(scanner.hasNext()){
-			String line = scanner.nextLine();
-			String[] lineSplits = line.split(",");
-			if(lineSplits.length == 2){
-				//the replace all removes spaces as well as non visible characters.
-				this.rooms.put(lineSplits[0].replaceAll("\\s+","").charAt(0), lineSplits[1].replaceAll("\\s+",""));
+		finally{
+			int clueLegendLinesRead = 1;//keep a counter for throwing errors for specific lines.
+			while(scanner.hasNext()){
+				String line = scanner.nextLine();
+				String[] lineSplits = line.split(",");
+				if(lineSplits.length == 2){
+					//the replace all removes spaces as well as non visible characters.
+					this.rooms.put(lineSplits[0].replaceAll("\\s+","").charAt(0), lineSplits[1].replaceAll("\\s+",""));
+				}
+				else{
+					throw new BadConfigFormatException(clueLegendFile + " is improperly formatted at line " + clueLegendLinesRead + ".");
+					//if the size does not equal 2, then there is more than one comma. Should we throw BadConfigFormatException here? Probably.
+				}
+				//increment error tracking counter
+				clueLegendLinesRead++;
 			}
-			else{
-				throw new BadConfigFormatException(clueLegendFile + " is improperly formatted at line " + clueLegendLinesRead + ".");
-				//if the size does not equal 2, then there is more than one comma. Should we throw BadConfigFormatException here? Probably.
+			try {
+				clueLegendFileReader.close();
+			} catch (IOException e) {
+				System.out.println("Exception: File: " + clueLegendFile + " did not close properly.\nAttempting to continue..");
+				System.exit(0);
 			}
 		}
 	}
@@ -81,10 +137,10 @@ public class Board {
 	}
 
 	public int getNumRows(){
-		return 0;
+		return numRows;
 	}
 	public int getNumColumns(){
-		return 0;
+		return numColumns;
 	}
 
 
